1c945fce50872d63f9552699a823455ba07b57a2
Alexander Zent <azent@gmx.de>
2018-05-29 14:55:07 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,29 @@
+Starten:
+--
+ng serve
+... und im Browser aubrufen
+
+
+
+In diesem Eval-Projekt geht es um 
+-----
+* two-way-databinding
+* event-processing
+* Referenz / Zugriff auf View-Bausteine
+
+
+
+app.component.html
+----
+* hier ist eine Input-Box eingebunden. 
+* wird ein "input"-Event ausgelöst, so wird es an an AppComponent.textInputEventProcessing() weiter gegeben
+
+app.components.ts
+----
+* Die Events kommen hier in AppComponent.textInputEventProcessing() an und werden entsprechend verarbeitet
+
+
+
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'app';
+
+  //
+  // Two way data binding demonstration
+  //
+
+  fg = "#00ffff";
+  bg = "#0000ff";
+
+  //
+  // Event-Demonstration
+  //
+
+  upperCase: string = '';
+  lowerCase: string = '';
+
+  // Referenz auf ein View-Baustein
+  @ViewChild('inputFieldMyRef') inputBox;
+  
+  // wird bei einem input-Event aufgerufen
+  textInputEventProcessing(event){
+    this.upperCase = event.target.value.toUpperCase();
+    this.lowerCase = event.target.value.toLowerCase();
+  }
+
+  // lifecycle method
+  ngAfterViewInit() {
+    this.inputBox.nativeElement.focus();
+  }
+
 }
